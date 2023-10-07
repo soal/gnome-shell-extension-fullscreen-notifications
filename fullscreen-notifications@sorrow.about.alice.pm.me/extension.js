02ff1c90bf2b2d5674a394bdf136216449456ab7
soal <soal@soal.red>
2023-10-07 16:24:51 +0300

    Updated for GNOME 44

--- fullscreen-notifications@sorrow.about.alice.pm.me/extension.js
+++ fullscreen-notifications@sorrow.about.alice.pm.me/extension.js
@@ -37,7 +37,7 @@ const Urgency = {
     HIGH: 2,
     CRITICAL: 3,
 };
-// Adopted from original Gnome Shell code. Original version can be found in gnome-shell/ui/messageTray.js
+// Adopted from original Gnome Shell code. Original version canbe found in gnome-shell/ui/messageTray.js
 function updateState() {
     let hasMonitor = Main.layoutManager.primaryMonitor != null;
     this.visible = !this._bannerBlocked && hasMonitor && this._banner != null;
@@ -52,7 +52,7 @@ function updateState() {
     // Filter out acknowledged notifications.
     let changed = false;
     this._notificationQueue = this._notificationQueue.filter((n) => {
-        changed ||= n.acknowledged;
+        changed = changed || n.acknowledged;
         return !n.acknowledged;
     });
 
@@ -63,6 +63,7 @@ function updateState() {
     if (this._notificationState == State.HIDDEN) {
         let nextNotification = this._notificationQueue[0] || null;
         if (hasNotifications && nextNotification) {
+            // Removed fullscreen check
             // let limited = this._busy || Main.layoutManager.primaryMonitor.inFullscreen;
             let limited = this._busy;
             let showNextNotification =
@@ -71,10 +72,7 @@ function updateState() {
                 nextNotification.urgency == Urgency.CRITICAL;
             if (showNextNotification) this._showNotification();
         }
-    } else if (
-        this._notificationState === State.SHOWING ||
-        this._notificationState === State.SHOWN
-    ) {
+    } else if (this._notificationState == State.SHOWN) {
         let expired =
             (this._userActiveWhileNotificationShown &&
                 this._notificationTimeoutId == 0 &&
@@ -88,12 +86,10 @@ function updateState() {
         if (mustClose) {
             let animate = hasNotifications && !this._notificationRemoved;
             this._hideNotification(animate);
-        } else if (
-            this._notificationState === State.SHOWN &&
-            this._pointerInNotification
-        ) {
-            if (!this._banner.expanded) this._expandBanner(false);
-            else this._ensureBannerFocused();
+        } else if (this._pointerInNotification && !this._banner.expanded) {
+            this._expandBanner(false);
+        } else if (this._pointerInNotification) {
+            this._ensureBannerFocused();
         }
     }
 
